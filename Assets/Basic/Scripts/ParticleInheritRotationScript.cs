f61ca0c0e1cf490f7db17b3c70adf1508c370aa8
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-02-16 20:49:59 +0000

    Skull Enemy & Sounds (No AI)

---
+++ Assets/Basic/Scripts/ParticleInheritRotationScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleInheritRotationScript : MonoBehaviour
+{
+	public Transform InheritFrom;
+
+	void Update()
+	{
+		GetComponent<ParticleSystem>().startRotation3D += InheritFrom.eulerAngles;
+	}
+}
