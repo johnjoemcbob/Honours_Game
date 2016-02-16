999aedb3068d54c423c6d170e2c639ff5c69e9a6
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-02-16 17:33:03 +0000

    Folder Restructure, Texture DeRes, Enemy Explosion Redo

---
+++ Assets/Basic/Scripts/ExternalCullingVolumeScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// This script will destroy objects outside of the volume of the box collider related to it
+// NOTE: May need extra functionality for more than one volume, allowing for different shapes
+
+public class ExternalCullingVolumeScript : MonoBehaviour
+{
+	public List<GameObject> IgnoreObjects;
+
+	void OnTriggerExit( Collider other )
+	{
+		if ( !IgnoreObjects.Contains( other.gameObject ) )
+		{
+			Destroy( other.gameObject );
+		}
+	}
+}
